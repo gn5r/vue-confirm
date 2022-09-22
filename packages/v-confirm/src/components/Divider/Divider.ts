f254b292a5fb78bb80004b6612f4380d29d35270
gn5r <shangyuan.tuolang@gmail.com>
2022-09-22 17:05:18 +0900

    feat(Divider): テーマstyleとdark propsを追加。クラスをcomputedで定義

--- packages/v-confirm/src/components/Divider/Divider.ts
+++ packages/v-confirm/src/components/Divider/Divider.ts
@@ -6,10 +6,22 @@ import "./Divider.scss";
 
 export default Vue.extend({
   name: "divider",
+  props: {
+    dark: Boolean,
+  },
+  computed: {
+    classes(): object {
+      return {
+        light: !this.dark,
+        dark: this.dark,
+        divider: true,
+      };
+    },
+  },
   render(h): VNode {
     return h("hr", {
       attrs: { role: "separator", "area-orientation": "horizontal" },
-      class: "divider",
+      class: this.classes,
     });
   },
 });

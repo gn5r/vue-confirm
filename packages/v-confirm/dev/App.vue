f56d7cd6f19c48a0e66da40429d40af20e62371c
gn5r <shangyuan.tuolang@gmail.com>
2022-12-12 02:25:07 +0900

    chore: add playground

---
+++ packages/v-confirm/dev/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <main></main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "app",
+  model: {},
+  mixins: [],
+  props: {},
+  data: () => ({}),
+  methods: {},
+  created() {
+    //
+  },
+  computed: {},
+  watch: {},
+  components: {},
+});
+</script>
+
+<style scoped></style>

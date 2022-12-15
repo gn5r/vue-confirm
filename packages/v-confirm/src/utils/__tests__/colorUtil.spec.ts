bc83a57f4dc942b6efc981705aa2fbcbd8850acd
gn5r <shangyuan.tuolang@gmail.com>
2022-12-16 04:56:55 +0900

    test(utils): remove deleted function test case

--- packages/v-confirm/src/utils/__tests__/colorUtil.spec.ts
+++ packages/v-confirm/src/utils/__tests__/colorUtil.spec.ts
@@ -1,5 +1,4 @@
-import { isCssColor, setBackgroundColor, setTextColor } from "../colorUtil";
-import { VNodeData } from "vue/types/vnode";
+import { isCssColor } from "../colorUtil";
 
 describe("colorUtil.ts", () => {
   it("#fff is true", () => {
@@ -24,33 +23,5 @@ describe("colorUtil.ts", () => {
 
   it("undefined value is false", () => {
     expect(isCssColor()).toBe(false);
-  });
-
-  it("setBackgroundColor with css style", () => {
-    const data: VNodeData = {};
-    expect(setBackgroundColor("#fff", data)).toEqual({
-      style: { "background-color": "#fff" },
-    });
-  });
-
-  it("setBackgroundColor with class style", () => {
-    const data: VNodeData = {};
-    expect(setBackgroundColor("primary", data)).toEqual({
-      class: { primary: true },
-    });
-  });
-
-  it("setTextColor with css style", () => {
-    const data: VNodeData = {};
-    expect(setTextColor("#fff", data)).toEqual({
-      style: { color: "#fff" },
-    });
-  });
-
-  it("setTextColor with class style", () => {
-    const data: VNodeData = {};
-    expect(setTextColor("white-text", data)).toEqual({
-      class: { "white-text": true },
-    });
   });
 });

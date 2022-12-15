39a88f9a5bc73e0e3f1c12743f4b72d552a925fc
gn5r <shangyuan.tuolang@gmail.com>
2022-12-16 02:34:24 +0900

    chore: tsconfigをcreate-vueと似た形に変更

--- packages/v-confirm/vite.dev.config.ts
+++ packages/v-confirm/vite.dev.config.ts
@@ -1,10 +1,19 @@
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+
+const resolve = (dir = "") => path.resolve(__dirname, dir);
 
 export default defineConfig({
   mode: "development",
   root: "dev",
-  plugins: [vue()],
+  plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      "@": resolve("src/"),
+    },
+  },
   server: {
     port: 8080,
   },

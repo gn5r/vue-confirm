018cca1608627a73eb48a13bd092f6c38073ee35
gn5r <shangyuan.tuolang@gmail.com>
2022-12-17 04:30:58 +0900

    chore(docs): コメントアウトを解除

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
-// import { VConfirmPlugin } from "@gn5r/vue-confirm";
-// import "@gn5r/vue-confirm/dist/v-confirm.min.css";
+import { VConfirmPlugin } from "@gn5r/vue-confirm";
+import "@gn5r/vue-confirm/dist/v-confirm.min.css";
 
-createApp(App).mount("#app");
+createApp(App).use(VConfirmPlugin).mount("#app");

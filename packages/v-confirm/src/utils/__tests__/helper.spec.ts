05ff4bac1f606c1f5cbbf562496dce563831d736
gn5r <shangyuan.tuolang@gmail.com>
2022-12-24 09:45:04 +0900

    test(helper): refacort test code and add test case of convertToUnit

--- packages/v-confirm/src/utils/__tests__/helper.spec.ts
+++ packages/v-confirm/src/utils/__tests__/helper.spec.ts
@@ -1,8 +1,25 @@
-import { getSize } from "../helper";
+import { describe, it, expect } from "vitest";
+import { getSize, convertToUnit } from "../helper";
 
 describe("helper.ts", () => {
-  it("get only a number value", () => {
-    const value = "800px";
-    expect(getSize(value)).toBe(800);
+  it("should return proper value in getSize", () => {
+    expect(getSize()).toBe(undefined);
+    expect(getSize("")).toBe(undefined);
+    expect(getSize(null)).toBe(undefined);
+    expect(getSize(undefined)).toBe(undefined);
+
+    expect(getSize("800px")).toBe(800);
+  });
+
+  it("should return proper value in convertToUni", () => {
+    expect(convertToUnit()).toBe(undefined);
+    expect(convertToUnit("")).toBe(undefined);
+    expect(convertToUnit(null)).toBe(undefined);
+    expect(convertToUnit(undefined)).toBe(undefined);
+
+    expect(convertToUnit(800)).toBe("800px");
+    expect(convertToUnit(800, "pt")).toBe("800pt");
+    expect(convertToUnit("800px")).toBe("800px");
+    expect(convertToUnit("800rem")).toBe("800px");
   });
 });

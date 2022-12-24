2c42b29d0dbd1494204aa8360901ebf2c2fe4e40
gn5r <shangyuan.tuolang@gmail.com>
2022-12-24 11:44:20 +0900

    test(MdiIcon): refactor test code using vitest for vue3

---
+++ packages/v-confirm/src/components/MdiIcon/__tests__/MdiIcon.spec.tsx
@@ -0,0 +1,31 @@
+import { mount } from "@vue/test-utils";
+import { describe, it, expect, vi } from "vitest";
+import { MdiIcon } from "../MdiIcon";
+import { mdiClose } from "@mdi/js";
+
+describe("MdiIcon.ts", () => {
+  it("render a component", () => {
+    const wrapper = mount(MdiIcon, { slots: { default: mdiClose } });
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("render a dark component", () => {
+    const wrapper = mount(MdiIcon, {
+      props: { dark: true },
+      slots: { default: mdiClose },
+    });
+    expect(wrapper.html()).toMatchSnapshot();
+    expect(wrapper.find("button.dark").exists()).toBe(true);
+  });
+
+  it("should emitted click event", async () => {
+    const func = vi.fn();
+    const wrapper = mount(MdiIcon, {
+      slots: { default: mdiClose },
+      attrs: { onClick: func },
+    });
+    await wrapper.find("button").trigger("click");
+    expect(func).toHaveBeenCalledTimes(1);
+    expect(wrapper.emitted("click")).toBeTruthy();
+  });
+});

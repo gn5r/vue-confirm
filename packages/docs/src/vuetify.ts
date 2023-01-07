3d354d7c35e5817efbf1e9fb8781758456aab9c4
gn5r <shangyuan.tuolang@gmail.com>
2023-01-08 08:57:59 +0900

    docs: set default light theme

--- packages/docs/src/vuetify.ts
+++ packages/docs/src/vuetify.ts
@@ -9,4 +9,22 @@ export default createVuetify({
     locale: "ja",
     messages: { ja },
   },
+  theme: {
+    themes: {
+      light: {
+        variables: {
+          "medium-emphasis-opacity": 0.8,
+        },
+        colors: {
+          primary: "#1976D2",
+          secondary: "#424242",
+          accent: "#82B1FF",
+          error: "#FF5252",
+          info: "#2196F3",
+          success: "#4CAF50",
+          warning: "#FFC107",
+        },
+      },
+    },
+  },
 });

8db698a269303b69a9646aefa5d9ce90dc641b4f
gn5r <shangyuan.tuolang@gmail.com>
2023-01-09 00:22:17 +0900

    docs: add locales dir and index file. fix japanese locale name to ja-JP

--- packages/docs/src/plugins/i18n/index.ts
+++ packages/docs/src/plugins/i18n/index.ts
@@ -1,15 +1,9 @@
 import { createI18n } from "vue-i18n";
-import en from "./en.json";
-import ja from "./ja.json";
-
-const messages = {
-  en: en,
-  ja: ja,
-};
+import { en, ja } from "./locales";
 
 export default createI18n({
   legacy: false,
   locale: window.navigator.language,
   fallbackLocale: "en",
-  messages: Object.assign(messages),
+  messages: { en, "ja-JP": ja },
 });

ecbc1adc4240e0ce4a7c29d22c2f850ec8e3546c
gn5r <shangyuan.tuolang@gmail.com>
2023-01-08 09:01:26 +0900

    docs: add page components

---
+++ packages/docs/src/pages/VCApi.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card></v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VCApi",
+  props: {},
+  setup() {
+    //
+  },
+  components: {},
+});
+</script>
+
+<style scoped></style>
---
+++ packages/docs/src/pages/VCSass.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card></v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "VCSass",
+  props: {},
+  setup() {
+    //
+  },
+  components: {},
+});
+</script>
+
+<style scoped></style>

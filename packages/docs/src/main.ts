e0deedb01c6a9cb8104b7aaee494e485badf2084
gn5r <shangyuan.tuolang@gmail.com>
2023-01-08 17:29:03 +0900

    docs: sort import order, add vue-i18n

--- packages/docs/src/main.ts
+++ packages/docs/src/main.ts
@@ -1,12 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-free/js/all.min.js";
+import "@/styles/main.scss";
+
 import { VConfirmPlugin } from "@gn5r/vue-confirm";
 import "@gn5r/vue-confirm/dist/v-confirm.min.css";
-import vuetify from "@/vuetify";
-import "@/styles/main.scss";
-import router from "@/router";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+
+import vuetify from "@/plugins/vuetify";
+import router from "@/plugins/router";
+import i18n from "@/plugins/i18n";
 
 const app = createApp(App);
-app.use(router).use(vuetify).use(VConfirmPlugin).mount("#app");
+app.use(router).use(vuetify).use(VConfirmPlugin).use(i18n).mount("#app");

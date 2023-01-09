112188386c22449385b817706a7b281cf12a49fb
gn5r <shangyuan.tuolang@gmail.com>
2023-01-10 02:33:55 +0900

    docs: 日本語の翻訳を修正

--- packages/docs/src/plugins/i18n/locales/ja.ts
+++ packages/docs/src/plugins/i18n/locales/ja.ts
@@ -1,21 +1,21 @@
 export default {
   "props": {
-    "btn-align": "justify-content css property.\nAvailable options are start,center,end,space-between,space-around.",
-    "btns": "Action buttons",
-    "closeable": "show / hide flag of close icon to dialog",
-    "dark": "Enable dark theme",
-    "hide-header": "hidden default header",
-    "message": "Message text",
-    "model-value": "show / hide flag of dialog",
-    "no-actions-divider": "show / hide flag of between message and action divider",
-    "persistent": "Clicking outside of the element not deactivate it",
-    "title": "Title text",
-    "title-color": "Background color of header",
-    "title-text-color": "Text color of header",
-    "width": "Component width"
+    "btn-align": "CSSのjustify-contentプロパティ\nstart,center,end,space-between,space-aroundが有効値です",
+    "btns": "アクションボタン",
+    "closeable": "ヘッダーに閉じるボタンを表示/非表示にするフラグ",
+    "dark": "ダークテーマを有効",
+    "hide-header": "デフォルトで描画されるヘッダーを非表示",
+    "message": "メッセージテキスト",
+    "model-value": "ダイアログを表示/非表示にするフラグ",
+    "no-actions-divider": "メッセージとアクションボタン間の境界線を非表示にする",
+    "persistent": "ダイアログの外側をクリックしても閉じないようにする",
+    "title": "タイトルテキスト",
+    "title-color": "ヘッダーの背景色",
+    "title-text-color": "ヘッダーの文字色",
+    "width": "コンポーネントの横幅"
   },
   "events": {
-    "click:outside": "Emitted when clicking outside an active dialog",
-    "update:modelValue": "The updated bound model"
+    "click:outside": "ダイアログの外側をクリックしたときに発火します",
+    "update:modelValue": "更新されたバウンドモデル"
   }
 }

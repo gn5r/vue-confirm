e65380c113ab7da3ea66dd2dcbbd641989a98f34
gn5r <32511242+gn5r@users.noreply.github.com>
2023-01-10 22:42:36 +0900

    docs: separate header, drawer and footer (#106)

---
+++ packages/docs/src/components/app/AppFooter.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer
+    class="justify-center text-center bg-transparent"
+    :class="
+      $vuetify.theme.current.dark ? 'text-grey-lighten-2' : 'text-grey-darken-2'
+    "
+    height="24"
+    app
+    absolute
+  >
+    <span>Development by</span>
+    <a href="https://github.com/gn5r" target="_blank" rel="author,noopener"
+      >&copy;shangyuan.tuolang</a
+    >
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "app-footer",
+});
+</script>
+
+<style scoped></style>

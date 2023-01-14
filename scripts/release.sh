331a87d3c6f3d79490b22ca649ccff4c2740e6f1
gn5r <shangyuan.tuolang@gmail.com>
2023-01-14 22:02:30 +0900

    chore: review the publish to npm settings of GitHub Actions

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,5 +2,4 @@
 
 npm config set //registry.npmjs.org/:_authToken ${NODE_AUTH_TOKEN:?}
 npm whoami
-yarn run build
 yarn lerna publish from-package --dist-tag $(node ./scripts/parse-dist-tag.js) --yes --no-verify-access

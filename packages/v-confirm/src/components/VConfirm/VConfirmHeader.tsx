c124c71a4c00ca816b0066c7ab7383ee4458f97f
gn5r <shangyuan.tuolang@gmail.com>
2023-01-26 05:21:40 +0900

    feat(VConfirm): add header slot

--- packages/v-confirm/src/components/VConfirm/VConfirmHeader.tsx
+++ packages/v-confirm/src/components/VConfirm/VConfirmHeader.tsx
@@ -18,6 +18,7 @@ export const VConfirmHeader = defineComponent({
       default: "inherit",
     },
     closeable: Boolean,
+    title: String,
     ...useThemeProps(),
   },
   emits: ["click"],
@@ -28,33 +29,42 @@ export const VConfirmHeader = defineComponent({
       toRef(props, "textColor")
     );
 
-    return () => (
-      <header
-        class={["v-confirm__header", backgroundColorClasses.value]}
-        style={[backgroundColorStyles.value]}
-      >
-        <div
-          class={textColorClasses.value}
-          style={[{ backgroundColor: "transparent" }, textColorStyles.value]}
+    return () =>
+      slots.default ? (
+        slots.default({
+          attrs: {
+            class: [...backgroundColorClasses.value, ...textColorClasses.value],
+            style: { ...backgroundColorStyles.value, ...textColorStyles.value },
+          },
+          title: props.title,
+        })
+      ) : (
+        <header
+          class={["v-confirm__header", backgroundColorClasses.value]}
+          style={[backgroundColorStyles.value]}
         >
-          {slots.default?.()}
-        </div>
-        {props.closeable && <div class="v-confirm__spacer" />}
-        {props.closeable && (
-          <div class="v-confirm__close-icon">
-            <MdiIcon
-              color={props.textColor}
-              dark={props.dark}
-              onClick={(e: Event) => {
-                emit("click", e);
-              }}
-            >
-              {mdiClose}
-            </MdiIcon>
+          <div
+            class={textColorClasses.value}
+            style={[{ backgroundColor: "transparent" }, textColorStyles.value]}
+          >
+            {props.title}
           </div>
-        )}
-      </header>
-    );
+          {props.closeable && <div class="v-confirm__spacer" />}
+          {props.closeable && (
+            <div class="v-confirm__close-icon">
+              <MdiIcon
+                color={props.textColor}
+                dark={props.dark}
+                onClick={(e: Event) => {
+                  emit("click", e);
+                }}
+              >
+                {mdiClose}
+              </MdiIcon>
+            </div>
+          )}
+        </header>
+      );
   },
 });
 

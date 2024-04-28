30bb350d382e75cb125413755e4a801d31cbbf0c
gn5r <shangyuan.tuolang@gmail.com>
2024-04-29 07:41:16 +0900

    fix: fix eslint config for vue

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,28 @@
 import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { fileURLToPath } from "url";
+import path from "path";
+import { FlatCompat } from "@eslint/eslintrc";
 
-/**
- * @type {import("eslint").Linter.FlatConfig[]}
- */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  ...compat.extends("@vue/typescript/recommended"),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "warn",
+      "vue/multi-word-component-names": "warn",
     },
   },
   {
--- packages/docs/eslint.config.mjs
+++ packages/docs/eslint.config.mjs
@@ -1,32 +1,10 @@
 import config from "../../eslint.config.mjs";
-import pluginVue from "eslint-plugin-vue";
-import { FlatCompat } from "@eslint/eslintrc";
-import { fileURLToPath } from "url";
-import path from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-/**
- * @type {import("eslint").Linter.FlatConfig[]}
- */
 export default [
   ...config,
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
-    },
-  },
-  {
-    files: ["**/*/vue"],
-    ...pluginVue.configs["flat/essential"],
-    ...compat.extends("@vue/typescript/recommended"),
-    rules: {
-      "vue/multi-word-component-names": "warn",
     },
   },
 ];

3c336388db2404117e8bcefb27f912381e5308d6
gn5r <32511242+gn5r@users.noreply.github.com>
2024-05-02 03:07:04 +0900

    fix: use Import Assertions in `rollup.config.mjs` (#225)

---
+++ packages/v-confirm/eslint.config.mjs
@@ -0,0 +1,21 @@
+import config from "../../eslint.config.mjs";
+import babelParser from "@babel/eslint-parser";
+
+/**
+ * @type {import("eslint").Linter.FlatConfig[]}
+ */
+export default [
+  ...config,
+  {
+    files: ["**/*.mjs"],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          plugins: ["@babel/plugin-syntax-import-assertions"],
+        },
+      },
+    },
+  },
+];

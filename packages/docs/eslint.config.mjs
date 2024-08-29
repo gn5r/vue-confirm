021c7c4753fddeb2e124bc514938b9b2dcf0f9d3
gn5r <shangyuan.tuolang@gmail.com>
2024-08-29 21:28:45 +0900

    feat: ignore .vitepress/cache from eslint

--- packages/docs/eslint.config.mjs
+++ packages/docs/eslint.config.mjs
@@ -1,7 +1,13 @@
 import config from "../../eslint.config.mjs";
 
+/**
+ * @type {import("eslint").Linter.Config}
+ */
 export default [
   ...config,
+  {
+    ignores: [".vitepress/cache"],
+  },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",

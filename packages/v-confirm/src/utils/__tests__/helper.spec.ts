29568df3d022b978d3496a82097165a82b8873db
gn5r <shangyuan.tuolang@gmail.com>
2024-12-21 00:49:54 +0900

    test: fix description of test code pointed out by GitHub Copilot

--- packages/v-confirm/src/utils/__tests__/helper.spec.ts
+++ packages/v-confirm/src/utils/__tests__/helper.spec.ts
@@ -9,9 +9,10 @@ describe("helper.ts", () => {
     expect(getSize(undefined)).toBe(undefined);
 
     expect(getSize("800px")).toBe(800);
+    expect(getSize(800)).toBe(800);
   });
 
-  it("should return proper value in convertToUni", () => {
+  it("should return proper value in convertToUnit", () => {
     expect(convertToUnit()).toBe(undefined);
     expect(convertToUnit("")).toBe(undefined);
     expect(convertToUnit(null)).toBe(undefined);
@@ -22,5 +23,6 @@ describe("helper.ts", () => {
     expect(convertToUnit("800px")).toBe("800px");
     expect(convertToUnit("800rem")).toBe("800rem");
     expect(convertToUnit("80%")).toBe("80%");
+    expect(convertToUnit(800, "em")).toBe("800em");
   });
 });

ce8c21035a2b8a9b7872aca7aa2ea3a0e6df2db1
gn5r <via.gn5r@hotmail.com>
2026-02-12 05:32:53 +0900

    fix: fix publish script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-npm config set //registry.npmjs.org/:_authToken ${NODE_AUTH_TOKEN:?}
 npm whoami
-pnpm -r publish --tag $(node ./scripts/parse-dist-tag.js) --yes
+pnpm publish -r --tag $(node ./scripts/parse-dist-tag.js)
